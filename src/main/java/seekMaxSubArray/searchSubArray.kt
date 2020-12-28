ad22942f3f24336f150b73bb9d72d961c1e8a692
haxul <s.s.starodubov@yandex.ru>
2020-12-28 22:09:39 +0400

    max subarray

--- src/main/java/seekMaxSubArray/searchSubArray.kt
+++ src/main/java/seekMaxSubArray/searchSubArray.kt
@@ -6,7 +6,7 @@ fun main() {
     print(maxSubArray)
 }
 
-
+/** start  */
 fun findMaxSubArray(list: List<Int>, low: Int, high: Int): Triple<Int, Int, Int> {
     if (high == low) return Triple(low, high, list[low])
     val mid = (low + high) / 2
@@ -40,3 +40,30 @@ fun findMaxCrossingSubArray(list: List<Int>, low: Int, mid: Int, high: Int): Tri
     }
     return Triple(maxLeft, maxRight, leftSum + rightSum)
 }
+
+/** end */
+
+fun sumIfMore50(list: List<Int>): Int {
+    if (list.size == 1) return if (list[0] < 10) list[0] else 0
+    val mid = list.size / 2
+    val leftSum = sumIfMore50(list.subList(0, mid))
+    val rightSum = sumIfMore50(list.subList(mid, list.size))
+    return leftSum + rightSum
+}
+
+fun findMinValue(list: List<Int>): Int {
+    if (list.size == 1) return list[0]
+    val mid = list.size / 2
+    val minLeft = findMinValue(list.subList(0, mid))
+    val minRight = findMinValue(list.subList(mid, list.size))
+    return if (minLeft < minRight) minLeft else minRight
+}
+
+fun sumArray(nums: List<Int>): Int {
+    if (nums.size == 1) return nums[0]
+    val mid = nums.size / 2
+    val sumLeft = sumArray(nums.subList(0, mid))
+    val sumRight = sumArray(nums.subList(mid, nums.size))
+    return sumLeft + sumRight
+}
+
