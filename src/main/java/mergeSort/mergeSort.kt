b6f6389e2eb2d974a1eef210015f02ff6499a8a0
haxul <s.s.starodubov@yandex.ru>
2020-12-28 18:14:07 +0400

    dfs

--- src/main/java/mergeSort/mergeSort.kt
+++ src/main/java/mergeSort/mergeSort.kt
@@ -1,7 +1,7 @@
 package mergeSort
 
 fun main() {
-    val bx = listOf(2, 8, 11, 12, 3, 1 ,15, 24,40, 0)
+    val bx = listOf(2, 8, 11, 12, 3, 1, 15, 24, 40, 0)
     val mergeSort = mergeSort(bx)
     println(mergeSort);
 }
@@ -10,14 +10,14 @@ fun main() {
 fun mergeSort(xs: List<Int>): List<Int> {
     if (xs.size == 1) return xs
     val ax = mergeSort(xs.subList(0, xs.size / 2))
-    val bx = mergeSort(xs.subList(xs.size/2, xs.size))
+    val bx = mergeSort(xs.subList(xs.size / 2, xs.size))
     return merge(ax, bx)
 }
 
 fun merge(ax: List<Int>, bx: List<Int>): List<Int> {
     val maxLen = ax.size + bx.size
     val sortList = mutableListOf<Int>()
-    var (i, j) = Pair(0,0)
+    var (i, j) = Pair(0, 0)
 
     for (inx in 0 until maxLen) {
         if (i >= ax.size) {
