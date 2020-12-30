c8736a497a2d65dc2bef9a57b6394f0601339fd8
starodubov <s.s.starodubov@yandex.com>
2020-12-30 21:08:24 +0400

    df

---
+++ src/main/java/heapSort/heapSort.kt
@@ -0,0 +1,33 @@
+package heapSort
+
+fun main() {
+    val list = mutableListOf(2, 12, 23, 4, 10, 2, 100, 101,102)
+    buildMaxHeap(list)
+    print(list)
+}
+
+/** heap methods */
+
+fun parent(i: Int): Int = i / 2
+fun left(i: Int): Int = 2 * i
+fun right(i: Int): Int = (2 * i) + 1
+
+fun buildMaxHeap(A: MutableList<Int>) {
+    val mid = A.size / 2
+    for (i in mid downTo 0) {
+        maxHeapify(A, i)
+    }
+}
+
+fun maxHeapify(A: MutableList<Int>, i: Int) {
+    val l = left(i)
+    val r = right(i)
+    var largest: Int
+
+    largest = if (l < A.size && A[l] > A[i]) l else i // if left children is more than parent
+    if (r < A.size && A[r] > A[largest]) largest = r
+    if (largest != i) {
+        A[i] = A[largest].also { A[largest] = A[i] }
+        maxHeapify(A, largest)
+    }
+}
