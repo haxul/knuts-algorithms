a658813f0e5f6a4f72713da23130e59db1c0423f
haxul <s.s.starodubov@yandex.ru>
2021-01-13 19:23:34 +0400

    max subarray

---
+++ src/main/java/bucketSort/Main.java
@@ -0,0 +1,43 @@
+package bucketSort;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        float[] arr = new float[]{ 0.897f, 0.565f, 0.656f, 0.1234f, 0.665f, 0.3434f };
+        bucketSort(arr, arr.length);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void bucketSort(float arr[], int n) {
+        if (n <= 0)
+            return;
+
+        // 1) Create n empty buckets
+        @SuppressWarnings("unchecked")
+        Vector<Float>[] buckets = new Vector[n];
+
+        for (int i = 0; i < n; i++) {
+            buckets[i] = new Vector<Float>();
+        }
+
+        // 2) Put array elements in different buckets
+        for (int i = 0; i < n; i++) {
+            float idx = arr[i] * n;
+            buckets[(int)idx].add(arr[i]);
+        }
+
+        // 3) Sort individual buckets
+        for (int i = 0; i < n; i++) {
+            Collections.sort(buckets[i]);
+        }
+
+        // 4) Concatenate all buckets into arr[]
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < buckets[i].size(); j++) {
+                arr[index++] = buckets[i].get(j);
+            }
+        }
+    }
+}
---
+++ src/main/java/bucketSort/bucketSort.kt
@@ -0,0 +1,16 @@
+package bucketSort
+
+
+fun main() {
+
+}
+
+fun bucketSort(inp:List<Int>) {
+    val n = inp.size - 1
+    val out = mutableListOf<MutableList<Int>>()
+    for (i in inp) {
+        out.add(mutableListOf())
+    }
+
+
+}
