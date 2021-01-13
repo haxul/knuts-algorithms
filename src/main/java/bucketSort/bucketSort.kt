f4439e47db8894e7fb5de8dbbb625eda2cc31d51
haxul <s.s.starodubov@yandex.ru>
2021-01-13 19:40:19 +0400

    bucket sort

--- src/main/java/bucketSort/bucketSort.kt
+++ src/main/java/bucketSort/bucketSort.kt
@@ -1,16 +1,34 @@
 package bucketSort
 
+import java.util.*
+
 
 fun main() {
-
+    val list = mutableListOf(0.11, 0.01, 0.03)
+    bucketSort(list)
+    println(list)
 }
 
-fun bucketSort(inp:List<Int>) {
-    val n = inp.size - 1
-    val out = mutableListOf<MutableList<Int>>()
+fun bucketSort(inp: MutableList<Double>) {
+    val n = inp.size
+    val buckets = mutableListOf<MutableList<Double>>()
     for (i in inp) {
-        out.add(mutableListOf())
+        buckets.add(mutableListOf())
     }
 
+    for (i in 0 until buckets.size) {
+        val inx: Int = (inp[i] * n).toInt()
+        buckets[inx].add(inp[i])
+    }
 
+    for (bucket in buckets) {
+        bucket.sort()
+    }
+
+    var index = 0
+    for (i in 0 until n) {
+        for (j in 0 until buckets[i].size) {
+            inp[index++] = buckets[i][j]
+        }
+    }
 }
