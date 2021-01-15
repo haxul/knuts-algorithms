ec97fbd59a9cd0c07addc40130f452e52d2dfff2
haxul <s.s.starodubov@yandex.ru>
2021-01-15 20:29:59 +0400

    binary tree

---
+++ src/main/java/binaryTree/Main.java
@@ -0,0 +1,27 @@
+package binaryTree;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+// binary tree
+
+class Tree {
+
+}
+
+
+class Node {
+
+    public Node(int id) {
+        this.id = id;
+    }
+
+    public int id;
+    public Node parent;
+    public Node left;
+    public Node right;
+}
